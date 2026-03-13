eba95690bbca110f5bb6b568a0a5d8a332677c4c
Ivan <9anvan@gmail.com>
2026-03-13 17:39:28 +0300

    enable cors support

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import userRoutes from './routes/userRoutes';
-import productRoutes from './routes/productRoutes';
-import cartRoutes from './routes/cartRoutes';
-import orderRoutes from './routes/orderRoutes';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
-export const app = express();
+import userRoutes from './routes/userRoutes'
+import productRoutes from './routes/productRoutes'
+import cartRoutes from './routes/cartRoutes'
+import orderRoutes from './routes/orderRoutes'
 
-app.use(express.json());
-app.use(cookieParser());
+export const app = express()
 
-app.use('/api/users', userRoutes);
+app.use(cors())
 
-app.use('/api/products', productRoutes);
+app.use(express.json())
+app.use(cookieParser())
 
-app.use('/api/cart', cartRoutes);
-
-app.use('/api/order', orderRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/cart', cartRoutes)
+app.use('/api/order', orderRoutes)
 
 app.get('/', (req, res) => {
-  res.json({ message: 'API is running' });
-});
+  res.json({ message: 'API is running' })
+})
